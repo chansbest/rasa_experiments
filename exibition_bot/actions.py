d7876032a1110f451a7bee14aab6f2d67e29fb5c
chansbest <chans.best@hotmail.com>
2019-04-28 10:23:04 +0530

    initial check in for exibition bot

---
+++ exibition_bot/actions.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import logging
+import requests
+import json
+from rasa_core_sdk import Action
+
+logger = logging.getLogger(__name__)
+
+
+class ActionJoke(Action):
+    def name(self):
+        # define the name of the action which can then be included in training stories
+        return "action_joke"
+
+    def run(self, dispatcher, tracker, domain):
+        # what your action should do
+        request = json.loads(
+            requests.get("https://api.chucknorris.io/jokes/random").text
+        )  # make an api call
+        joke = request["value"]  # extract a joke from returned json response
+        dispatcher.utter_message(joke)  # send the message back to the user
+        return []
+
+
+
+class ActionEnqiry(Action):
+    def name(self):
+        return "action_enquiry"
+    
+    def run(self, dispatcher, tracker, domain):
+       
+        paper_type = tracker.get_slot('session_topic')
+        print(paper_type)
+        dispatcher.utter_message("you just did an session enquiry")
+        
+        return []
